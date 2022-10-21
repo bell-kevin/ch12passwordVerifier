c45e447e17db605df5457c9537d479f7f3c928bb
Kevin Bell <bellKevin@pm.me>
2022-10-21 14:35:28 -0600

    Add files via upload

---
+++ ch12passwordVerifier/ch12passwordVerifier/ch12passwordVerifier.cpp
@@ -0,0 +1,54 @@
+// ch12passwordVerifier.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Chapter 12 Password Verifier by Kevin Bell\n\n";
+	cout << "This program checks a password to ensure it meets our specifications for secure passwords.\n";
+	cout << "Enter a password to check: ";
+	string password;
+	cin >> password;
+	int minPasswordLength = 6;
+	int length = password.length();
+	int upper = 0;
+	int lower = 0;
+	int digit = 0;
+	for (int i = 0; i < length; i++)
+	{
+		if (isupper(password[i]))
+		{
+			upper++;
+		}
+		else if (islower(password[i]))
+		{
+			lower++;
+		}
+		else if (isdigit(password[i]))
+		{
+			digit++;
+		}
+	} // end for loop
+	if (length < minPasswordLength)
+	{
+		cout << "The password must be at least 6 characters long.\n";
+	}
+	if (upper == 0)
+	{
+		cout << "The password must contain at least one uppercase letter.\n";
+	}
+	if (lower == 0)
+	{
+		cout << "The password must contain at least one lowercase letter.\n";
+	}
+	if (digit == 0)
+	{
+		cout << "The password must contain at least one digit.\n";
+	}
+	if (length >= minPasswordLength && upper > 0 && lower > 0 && digit > 0)
+	{
+		cout << "The password is valid. Congratulations!\n";
+	} // end if
+} // end main
